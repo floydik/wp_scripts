beb72382c7c2153fa1b93be59593530bf1c8f191
Zbynek Vyboh <zbynek@vyboh.net>
2022-09-30 12:34:38 +0200

    Update insertadmin.php

--- insertadmin.php
+++ insertadmin.php
@@ -18,16 +18,16 @@ $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
         die('Connect Error (' . mysqli_connect_errno() . ') '
            . mysqli_connect_error());
         }
-$q = "INSERT INTO `'.$table_prefix.'_users` (`user_login`, `user_pass`, `user_nicename`, `user_email`, `user_status`)
+$q = "INSERT INTO `".$table_prefix."_users` (`user_login`, `user_pass`, `user_nicename`, `user_email`, `user_status`)
 VALUES ('$user_login', MD5('$user_pass'), '$user_nicename', '$user_email', '0');";
 echo $q."<br /n>";
 
-$q = "INSERT INTO `'.$table_prefix.'_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) 
-VALUES (NULL, (Select max(id) FROM '.$table_prefix.'_users), 'wp_capabilities', 'a:1:{s:13:\"administrator\";s:1:\"1\";}');";
+$q = "INSERT INTO `".$table_prefix."_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) 
+VALUES (NULL, (Select max(id) FROM ".$table_prefix."_users), 'wp_capabilities', 'a:1:{s:13:\"administrator\";s:1:\"1\";}');";
 echo $q."<br /n>";
 
-$q = "INSERT INTO `'.$table_prefix.'_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) 
-VALUES (NULL, (Select max(id) FROM '.$table_prefix.'_users), 'wp_user_level', '10');";
+$q = "INSERT INTO `".$table_prefix."_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) 
+VALUES (NULL, (Select max(id) FROM ".$table_prefix."_users), 'wp_user_level', '10');";
 echo $q."<br /n>";
 /*
 if ($mysqli->query($q) === TRUE) {
