8f17e3245f222749973f4e84e45b0903655f2cd3
Zbynek Vyboh <zbynek@vyboh.net>
2022-09-30 13:02:39 +0200

    Update insertadmin.php

--- insertadmin.php
+++ insertadmin.php
@@ -1,15 +1,16 @@
 <?php
 /* insert admin account itno WP database */
 
-// channge to real values:
+// channge flowing to real values:
 // user login
-$user_login = "floydik";
+$userlogin = "floydik";
 // user nice name
-$user_nicename = "Floydik from Brno";
+$usernicename = "Floydik from Brno";
 // user password
-$user_pass = "IbHaHer\drynluWumeedvib8";
+$userpass = "IbHaHer\drynluWumeedvib8";
 // user e-mail
-$user_email = "someone@somewhere.tld";
+$useremail = "someone@somewhere.tld";
+//
 
 require ("wp-config.php");
 
@@ -19,19 +20,21 @@ $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
            . mysqli_connect_error());
         }
 $q = "INSERT INTO `".$table_prefix."users` (`user_login`, `user_pass`, `user_nicename`, `user_email`, `user_status`)
-VALUES ('$user_login', MD5('$user_pass'), '$user_nicename', '$user_email', '0');";
-echo $q."<br /n>";
-
-$q = "INSERT INTO `".$table_prefix."usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) 
-VALUES (NULL, (Select max(id) FROM ".$table_prefix."users), 'wp_capabilities', 'a:1:{s:13:\"administrator\";s:1:\"1\";}');";
-echo $q."<br /n>";
-
-$q = "INSERT INTO `".$table_prefix."usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) 
-VALUES (NULL, (Select max(id) FROM ".$table_prefix."users), 'wp_user_level', '10');";
-echo $q."<br /n>";
-/*
+VALUES ('".$userlogin."', MD5('".$userpass."'), '".$usernicename."', '".$useremail."', '0');";
 if ($mysqli->query($q) === TRUE) {
-                printf("INSERT OK\n");
+                printf("INSERT OK<br />");
 }
-*/
+
+$q = "INSERT INTO `".$table_prefix."usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`)
+VALUES (NULL, (Select max(id) FROM ".$table_prefix."users), 'wp_capabilities', 'a:1:{s:13:\"administrator\";s:1:\"1\";}');";
+if ($mysqli->query($q) === TRUE) {
+                printf("INSERT OK<br />);
+}
+
+$q = "INSERT INTO `".$table_prefix."usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`)
+VALUES (NULL, (Select max(id) FROM ".$table_prefix."users), 'wp_user_level', '10');";
+if ($mysqli->query($q) === TRUE) {
+                printf("INSERT <br />");
+}
+
 ?>
